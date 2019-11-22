e3851fcfc99442bc8d2f048056206de8cba00cd4
Student <example@example.com>
2019-11-22 11:19:46 -0800

    updated display logic

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './styles.css';
 
 
 $(document).ready(function() {
-  
+
   (async () => {
     let doctorService = new DoctorService();
     const response = await doctorService.getDoctors("pain");
@@ -21,22 +21,41 @@ $(document).ready(function() {
         ${doctor.profile.first_name} ${doctor.profile.last_name}
         </div> <!-- class = card-header -->
         <div class="card-body">
-        Specialies: ${doctor.specialties[0].actor}
-        <ul class="list-group list-group-flush" id="${doctor.profile.first_name}${doctor.profile.last_name}">
-        </ul>
+        Specialies:
+        <ul class="list-group list-group-flush" id="${doctor.npi}"></ul>
+        Practices:
+        <ul class="list-group list-group-flush" id="${doctor.profile.first_name}${doctor.profile.last_name}"></ul>
         </div> <!-- class = card-body -->
         <div class="card-footer">
 
         </div> <!-- class = card-footer -->
         </div> <!-- class = card -->
       `);
+      doctor.specialties.forEach(function(specialty) {
+        $("#"+doctor.npi).append(`
+          <li class="list-group-item">
+            ${specialty.name}
+          </li>
+          <li class="list-group-item">
+            ${specialty.description}
+          </li>
+        `)
+      });
       doctor.practices.forEach(function(practice) {
         $("#"+doctor.profile.first_name+doctor.profile.last_name).append(`
           <li class="list-group-item">
           Practice: ${practice.name} <br>
           Location: ${practice.visit_address.street}, ${practice.visit_address.city} ${practice.visit_address.state}, ${practice.visit_address.zip}<br>
+          Phone Numbers:
+          <ul class="list-group list-group-flush" id="${practice.uid}">
+          </ul>
           Accepting New Patients: ${practice.accepts_new_patients}
         `);
+        practice.phones.forEach(function(phone) {
+          $("#"+practice.uid).append(`
+            <li class="list-group-item> ${phone.type}: ${phone.number}"
+          `);
+        });
       });
     });
     console.log(response);
